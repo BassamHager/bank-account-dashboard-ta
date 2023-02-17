e8107e57266e4de3cec94ce9cf9879c09acbe9c5
Bassam <b2dndr1@gmail.com>
2023-02-17 23:58:00 +0100

    feat: create get-accounts service - add mock data - accounts.json - create service to fetch accounts - update home page to execute the fetch on-mounted

--- try1/src/views/HomeView.vue
+++ try1/src/views/HomeView.vue
@@ -1,4 +1,25 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { onMounted, ref } from "vue";
+// services
+import { getAccountsService } from "@/services/getAccounts";
+// types
+import type { IAccountGroup } from "@/types/typings.d.ts";
+
+// state //
+const accounts = ref<IAccountGroup[]>();
+
+// methods //
+onMounted(async () => {
+  const fetchedData = await getAccounts();
+  accounts.value = fetchedData.accountGroups;
+  console.log(accounts.value);
+});
+
+const getAccounts = async () => {
+  const fetchedAccounts = await getAccountsService();
+  return fetchedAccounts;
+};
+</script>
 
 <template>
   <main>
