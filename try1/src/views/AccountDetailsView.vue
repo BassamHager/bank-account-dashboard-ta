5af0e1ce2dad25e51bb365c209fa6b57eb4cb4da
Bassam <b2dndr1@gmail.com>
2023-02-22 18:22:27 +0100

    wip: cleanup AccountDetailsView

--- try1/src/views/AccountDetailsView.vue
+++ try1/src/views/AccountDetailsView.vue
@@ -1,34 +1,12 @@
 <script setup lang="ts">
-import { ref, onMounted } from "vue";
 import { useRoute } from "vue-router";
 // components
 import AccountDetails from "@/components/accounts/AccountDetails.vue";
-// types
-import type { IAccountStatement } from "@/types/transaction";
-// services
-import { getTransactionsByAccountNumber } from "@/services/getTransactions";
-import { getAccountBalance, getAccountsService } from "@/services/getAccounts";
-// data
-const accountStatement = ref<IAccountStatement>();
-const accountBalance = ref<number>();
 // router & routes
 const route = useRoute();
 const { accountId } = route.params;
-// methods
-onMounted(async () => {
-  accountStatement.value = await getTransactionsByAccountNumber({
-    accountNumber: accountId as string,
-  });
-  accountBalance.value = await getAccountBalance({
-    accountNumber: accountId as string,
-  });
-});
 </script>
 
 <template>
-  <AccountDetails
-    v-if="accountStatement"
-    :accountStatement="accountStatement"
-    :balance="accountBalance"
-  />
+  <AccountDetails :accountId="accountId" />
 </template>
